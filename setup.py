b3df67fc4aef62f56ee70570d4f05bc8bea9280a
wuhui2023 <18896724614@139.com>
2023-10-24 10:18:11 +0800

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-editor",
-    version="0.1.0",
+    version="v0.0.1",
     author="wuhui",
     author_email="18896724614@139.com",
     description="A components allow editor words",
